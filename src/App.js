919539532d2247a9db6a9e835026d76aa55bda9d
anshubaldeep <anshubaldeep@gmail.com>
2020-11-12 04:24:46 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {Navbar , NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-        </Navbar>      
+        </Navbar>
+        <Menu/>      
     </div>
   );
 }
